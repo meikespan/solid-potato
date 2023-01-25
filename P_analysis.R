37c8f5c00a0e22d0e3361090c3b7ed8a64b48fb2
Priyal Ranka <priyalranka2609@gmail.com>
2023-01-25 17:05:30 +0100

    saving bar graph, the second attempt

--- P_analysis.R
+++ P_analysis.R
@@ -5,14 +5,14 @@ uni_people<-read_csv("uni_people.csv")
 
 #consolidating the data-set to include a separate column for the number of people in each occupational group
 profession_N <- uni_people |>
-  group_by(`Occupational group`) |>
+  group_by(`OccupationalGroup`) |>
   summarise(number_of_people=n()) |>
 #calculating the proportion of all the university alumni that each occupational group makes
   mutate(percentage=number_of_people / sum(number_of_people))
 
 #Visualizing the above calculation in the form of a bar graph
 bargraph_occupation_uni_percentage=ggplot(data=profession_N)+
-  aes(x=`Occupational group`, y=percentage)+
+  aes(x=`OccupationalGroup`, y=percentage)+
   labs(title="Percentage of college alumni per occupational group")+
   labs(y="Percentage of college alumni", x="Occupational group")+
   scale_y_continuous (labels = scales::label_percent())+
@@ -23,9 +23,12 @@ bargraph_occupation_uni_percentage=ggplot(data=profession_N)+
 
 print(bargraph_occupation_uni_percentage)
 
+
 line_graph_over_the_years=ggplot(data=uni_people)+
   aes(x=Title, y="Occupational group")+
   geom_point()
+
+
 
 
 
