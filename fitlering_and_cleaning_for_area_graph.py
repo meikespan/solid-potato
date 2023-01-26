f3bab3f3443b6b1aa6da4d6f1c14e947ec876077
“Meike <m.e.j.span@students.uu.nl>
2023-01-26 15:31:10 +0100

    finished visualization script - cleaned up files

---
+++ fitlering_and_cleaning_for_area_graph.py
@@ -0,0 +1,128 @@
+import json
+import csv
+
+people_uni_in_total = []
+excluded_no = 0
+people_output = []
+
+#defining functions
+def extract_entry(entry, label):
+    return {
+        'Title': entry['title'],
+        'Birthyear': entry.get('ontology/birthYear'),
+        'Occupational_group': label
+    }
+
+def check_labels(look_heres, labels):
+    for label in labels:
+        for look_here in look_heres:
+            if label in look_here:
+                return True
+
+#defining the labels for each occupation
+label_mapping = {
+    'athlete': ['athlet', 'coach', 'sport', 'ball'], 
+    'academic': ['scienti', 'professor', 'economist', 'philosopher', 'science', 'intellectual', 'histor', 'linguist', 'chem', 'philos', 'astronomy', 'scholar', 'math', 'engineer', 'biolog'],
+    'office_holder': ['office holder', 'president', 'chairman', 'executive', 'mayor', 'united nations'], 
+    'politician':['politician', 'political', 'minist', 'parliament', 'mayor', 'polit', 'diplomat'],
+    'religious_figure' : ['bishop', 'pope', 'christian', 'rabbi', 'church', 'faith', 'buddhis'],
+}
+
+
+
+#----------------------------------------------------------------------------------------------------------------------------------------------
+#filtering data - includes only real non-criminals that list a university
+
+for i in ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']:
+
+    # Load JSON list of dictionaries
+    with open(f'People/{i}_people.json') as file:
+        people_all = json.load(file)
+
+    people_uni = []
+    people_uni_in = []
+
+  #filtering for uni education
+    for entry in people_all:
+        is_educated = False
+        for key in entry:
+            if type(entry[key]) is list:
+                if any(('university' in value.lower() or 'college' in value.lower()) for value in entry[key]):
+                    is_educated = True
+            else:
+                if 'university' in entry[key].lower() or 'college' in entry[key].lower():
+                    is_educated = True
+        if is_educated:
+            entry['http://www.w3.org/1999/02/22-rdf-syntax-ns#type_label'] = [item for item in entry['http://www.w3.org/1999/02/22-rdf-syntax-ns#type_label'] if (item != 'person') and (item != 'Person') and (item != 'agent') and (item != 'owl#Thing') and (not item.startswith('DUL.owl#')) and (not item.startswith('Q'))]
+            people_uni.append(entry)
+        else:
+            excluded_no += 1
+
+
+    #filtering out all non-persons and criminals
+    for entry in people_uni:
+        is_fictional = any('fictional' in label.lower() for label in entry["http://www.w3.org/1999/02/22-rdf-syntax-ns#type_label"])
+        is_criminal = any('criminal' in label.lower() for label in entry["http://www.w3.org/1999/02/22-rdf-syntax-ns#type_label"])
+        
+        if is_fictional or is_criminal:
+            excluded_no += 1
+        else:
+            people_uni_in.append(entry)
+
+
+    people_uni_in_total += people_uni_in
+
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ #defining their occupational groups
+
+#defining the labels in our original dictionary that might contain occupational info and storing this as individual list items to be checked
+occupation_keys = ['ontology/occupation_label','ontology/field_label', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type_label']
+
+for entry in people_uni_in_total:
+    searcharea = []
+    for key in occupation_keys:
+        if key in entry:
+            if type(entry[key]) is list:
+                searcharea.extend(entry[key])
+            elif entry[key] != '':
+                searcharea.append(entry[key])
+    
+    searcharea = [searchareum.lower() for searchareum in searcharea if len(searchareum) >0 and not searchareum[-1].isdigit()] #lowercasing our potential occupations #and removing info of the form ___digit that are not informational in our data
+
+    #fixing some problems in the data where sometimes the birthyear is a list with the first entry not being a likely year but the second is
+    if 'ontology/birthYear' in entry:
+        if type(entry['ontology/birthYear']) is list:
+            entry['ontology/birthYear'] = entry['ontology/birthYear'][1]
+            
+    
+    
+    #doing our actual checks
+    has_occupation = False
+    for occupation, labels in label_mapping.items():
+        if check_labels(searcharea, labels):
+            people_output.append(extract_entry(entry, occupation))
+            has_occupation = True
+
+    if not has_occupation and len(searcharea) != 0:
+        people_output.append(extract_entry(entry, 'other'))
+    
+
+names_list = [output['Title'] for output in people_output]
+unique_names = set(names_list) 
+#----------------------------------------------------------------------------------------------------------------------------------------------------
+#exporting into csv and checking the amounts of observations
+
+print('number excluded:', excluded_no)
+print('number included', len(people_uni_in_total))
+print(len(names_list))
+print(len(unique_names))
+ 
+# Write column headers and export our data into a usable csv
+with open('people_output_for_area_graph.csv', 'w') as csvfile:
+    fieldnames = ['Title', 'Birthyear', 'Occupational_group']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames, restval='', extrasaction='ignore')
+
+    writer.writeheader()
+    for person in people_output:
+        writer.writerow(person) 
---
+++ occ_uni_visualisation.R
@@ -0,0 +1,70 @@
+library(tidyverse)
+library(ggthemes)
+
+
+#reading in our data and transforming it into useful stuff - bar plot
+uni_people_data <- read_csv('people_output.csv') |>
+  filter(Birthyear <= 1990 & Birthyear >= 1500, na.rm = TRUE) |>
+  group_by(Occupational_group) |>
+  summarise(n = n())
+
+  total_uni <- sum(uni_people_data[, 'n'])
+
+#reading in our data and transforming it into useful stuff - area graph 
+people_data <- read_csv("people_output_for_area_graph.csv") |>
+  mutate(Birthyear = as.numeric(Birthyear)) |>
+  mutate(Decade = (Birthyear - (Birthyear %% 10)), na.rm=TRUE) |> #flooring each birth-year, leaving us with the decade
+  filter(Decade <= 1990 & Decade >= 1500) |> #filtering for our range of decades
+  group_by(Occupational_group, Decade) |>
+  summarise(number_of_people = n()) |> #counting the number of people per occupational group per decade
+  group_by(Decade) |>
+  mutate(proportion_people = number_of_people / sum(number_of_people)) # adding a column with the percentages
+          #calculated by dividing the number of people per group per decade by the total people per decade
+
+#reordering the order of the occupational groups so 'other' appears on top of the stack
+reordered_data <- people_data |>
+  mutate(Occupational_group = factor(Occupational_group))
+    
+
+#plotting respective output into a area graph
+(area_plot <- ggplot(data = reordered_data) +
+  aes(x = Decade, y = proportion_people, fill = Occupational_group, width = 10) +
+  labs(y = "Percentage of college degrees", x = "Decade", fill = "Occupational group") +
+  scale_y_continuous(breaks = seq(0,1, by = .1), minor_breaks = seq(0,1, by = .05), labels = scales::label_percent()) +
+  scale_x_continuous(breaks = seq(1500,2000, by = 50), minor_breaks = seq(1500,2000, by = 10))+
+  geom_bar(stat = "identity") +
+  scale_fill_brewer() +
+  theme_igray() +
+  theme(panel.grid.major.x = element_line(colour = "grey50",
+                                          size = 0.25,
+                                          linetype = 1),
+        panel.grid.minor.x = element_line(colour = "grey80",
+                                          size = 0.25,
+                                          linetype = 1),
+        panel.grid.major.y = element_line(colour = "grey50",
+                                          size = 0.25,
+                                          linetype = 1),
+        panel.grid.minor.y = element_line(colour = "grey80",
+                                          size = 0.25,
+                                          linetype = 1)))
+
+#plotting respective output into a bar plot
+occupation_bar_plot <- ggplot(data = uni_people_data) +
+  aes(x=reorder(Occupational_group,n/total_uni), y = n/total_uni) +
+  geom_col(fill = "#2171B5") +
+  coord_flip() +
+  scale_y_continuous(breaks = seq(0,1, by = .1), minor_breaks = seq(0,1, by = .01), labels = scales::label_percent(), limits=c(NA, .3)) +
+  labs(y = 'Percentage of total people with a degree', 
+       x = 'Occupational group')  +
+  scale_fill_brewer() +
+  theme_igray()+
+  theme(panel.grid.major.x = element_line(colour = "grey50",
+                                          size = 0.25,
+                                          linetype = 1),
+        panel.grid.minor.x = element_line(colour = "grey80",
+                                          size = 0.25,
+                                          linetype = 1))
+
+
+ggsave('occupation_bar_plot.pdf', plot = occupation_bar_plot, scale = 1, width=7, height=5)
+ggsave('decade_area_graph.pdf', plot = area_plot, scale = 1, width=7, height=5)
