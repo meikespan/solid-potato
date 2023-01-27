cfbc3729f855adb2fb50afbea4292007675593db
“Meike <m.e.j.span@students.uu.nl>
2023-01-27 17:23:32 +0100

    final version baybeee

--- occ_uni_visualisation.R
+++ occ_uni_visualisation.R
@@ -6,7 +6,7 @@ library(wesanderson)
 #reading in our data and transforming it into useful stuff - bar plot
 uni_people_data <- read_csv('people_output.csv') |>
   mutate(Birthyear = as.numeric(Birthyear)) |>
-  filter(Birthyear <= 1990 & Birthyear >= 1500, na.rm = TRUE) |>
+  filter(Birthyear <= 1999 & Birthyear >= 1500, na.rm = TRUE) |>
   group_by(Occupational_group) |>
   summarise(n = n()) |>
   mutate(highlight = c('1', '0', '0', '1', '0', '0', '0', '0', '0', '1', '0', '1', '1', '0'))
@@ -17,7 +17,7 @@ uni_people_data <- read_csv('people_output.csv') |>
 #reading in our data and transforming it into useful stuff - area graph 
 people_data <- read_csv("people_output_for_area_graph.csv") |>
   mutate(Birthyear = as.numeric(Birthyear)) |>
-  filter(Birthyear <= 1990 & Birthyear >= 1500) |>  #filtering for our range of decades
+  filter(Birthyear <= 1999 & Birthyear >= 1500) |>  #filtering for our range of decades
   mutate(Decade = (Birthyear - (Birthyear %% 10)), na.rm=TRUE) |> #flooring each birth-year, leaving us with the decade
   group_by(Occupational_group, Decade) |>
   summarise(number_of_people = n()) |> #counting the number of people per occupational group per decade |>
@@ -34,8 +34,8 @@ reordered_data <- people_data |>
 #plotting respective output into a area graph
 area_plot <- ggplot(data = reordered_data) +
   aes(x = Decade, y = proportion_people, fill = Occupational_group, width = 10) +
-  labs( y = "Percentage of college alumni", 
-        x = "Decade", 
+  labs( y = "Percentage of total college alumni", 
+        x = "Birth-year (by decade)", 
         fill = "Occupational group") +
   scale_y_continuous(
         breaks = seq(0,1, by = .1), 
@@ -67,7 +67,7 @@ occupation_bar_plot <- ggplot(data = uni_people_data) +
   geom_col() +
   coord_flip() +
   scale_y_continuous(
-          breaks = seq(0,1, by = .1), 
+          breaks = seq(0,1, by = .05), 
           minor_breaks = seq(0,1, by = .01), 
           labels = scales::label_percent(), 
           limits=c(NA, .3)) +
@@ -87,8 +87,8 @@ occupation_bar_plot <- ggplot(data = uni_people_data) +
 #plotting respective output into a faceted linegraph
 faceted_line_graph <- ggplot(data = reordered_data) +
   aes(x= Decade, y = proportion_people, color = Occupational_group) +
-  geom_line(linewidth=1) +
-  facet_grid(Occupational_group ~ .) +
+  geom_line(linewidth=1.2) +
+  facet_wrap(~Occupational_group, nrow = 3 ) +
   scale_color_manual(values = c('#ADB17DFF', '#B1746FFF', '#5B8FA8FF', '#FFB547FF', '#725663FF','#800000FF' )) +
   scale_y_continuous(
         breaks = seq(0,1, by = .2), 
@@ -99,23 +99,25 @@ faceted_line_graph <- ggplot(data = reordered_data) +
         breaks = seq(1500,2000, by = 100), 
         minor_breaks = seq(1500,2000, by = 20)) +
   labs(y = 'Percentage of total college alumni', 
-       x = 'Decade')  +
+       x = 'Birth-year (by decade)',
+       color = "Occupational group")  +
   theme_igray() +
-  theme(panel.grid.major.x = element_line(colour = "grey50",
-                                          size = 0.25,
+  theme(panel.grid.major.x = element_line(colour = "grey60",
+                                          size = 0.1,
                                           linetype = 1),
         panel.grid.minor.x = element_line(colour = "grey80",
-                                          size = 0.25,
+                                          size = 0.1,
                                           linetype = 1),
-        panel.grid.major.y = element_line(colour = "grey50",
-                                          size = 0.25,
+        panel.grid.major.y = element_line(colour = "grey60",
+                                          size = 0.1,
                                           linetype = 1),
         panel.grid.minor.y = element_line(colour = "grey80",
-                                          size = 0.25,
-                                          linetype = 1))
+                                          size = 0.1,
+                                          linetype = 1),
+        legend.position = "none")
 
 
 
 ggsave('occupation_bar_plot.pdf', plot = occupation_bar_plot, scale = 1, width=7, height=5)
 ggsave('decade_area_graph.pdf', plot = area_plot, scale = 1, width=7, height=5)
-ggsave('faceted_line_graph.pdf', plot = faceted_line_graph, scale = 1, width=5, height=7)
+ggsave('faceted_line_graph.pdf', plot = faceted_line_graph, scale = 1, width=7, height=5)
